4a1805fb36014be9a9de52a51a1e0ebfa209ba7f
Renan Ribeiro <63807288+renanribs@users.noreply.github.com>
2020-05-22 15:39:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -44,14 +44,11 @@ class Playlist():
         self.nome = nome
         self._programas = programas
 
-    @property
-    def listagem(self):
-        return self._programas
+    def __getitem__(self, item)
+        return self._programas[item]
 
-    @property
-    def tamanho(self):
+    def __len__(self):
         return len(self._programas)
-
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
@@ -71,7 +68,7 @@ demolidor.dar_likes()
 listinha = [atlanta, vingadores, demolidor, tmep]
 minha_playlist = Playlist('fim de semana', listinha)
 
-for programa in minha_playlist.listagem:
+for programa in minha_playlist:
     print(programa)
 
 print(f'Tamanho: {len(minha_playlist.listagem)}')
